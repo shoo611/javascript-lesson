09ad180765063aeaf4d38413be632df853149e39
shoo611 <itonagasho@itonagashounoMacBook-Air.local>
2025-09-02 11:26:09 +0900

    docs: Q2回答

--- question/app.js
+++ question/app.js
@@ -2,3 +2,8 @@
 let nickname = 'しょう';
 let age = 25;
 console.log('私のニックネームは' + nickname + 'です。年齢は' + age +'歳です。');
+
+// Q2 配列
+let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go',];
+let templateText = `私の好きな言語は${languages[0]}です。次は${languages[3]}を勉強してみたいです。`;
+console.log(templateText);
