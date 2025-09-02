3e03d9c1a6d8ce2636770a385bd0f14f3e05caca
shoo611 <itonagasho@itonagashounoMacBook-Air.local>
2025-09-02 11:38:37 +0900

    docs: Q4回答

--- question/app.js
+++ question/app.js
@@ -16,3 +16,23 @@ let user = {
   favorite: 'card',
 };
 console.log(user.age);
+
+// Q4  配列 × オブジェクト
+let playerList = [
+  {
+    name: 'John',
+    age: 26,
+    favorites: ['Card Game', 'Basket Ball', 'Programming'],
+  },
+  {
+    name: 'Bob',
+    age: 33,
+    favorites: ['Tinder', 'The Legend of Zelda'],
+  },
+  {
+    name: 'Michael',
+    age: 22,
+    favorites: ['Football', 'Smash Bros.'],
+  },
+];
+console.log(playerList[1].favorites[1]);
