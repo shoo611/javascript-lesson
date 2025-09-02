3337590762de058af931dacce0ce9392f6466ef8
shoo611 <itonagasho@itonagashounoMacBook-Air.local>
2025-09-02 11:28:52 +0900

    docs: Q3回答

--- question/app.js
+++ question/app.js
@@ -1,9 +1,18 @@
 // Q1 変数
 let nickname = 'しょう';
 let age = 25;
-console.log('私のニックネームは' + nickname + 'です。年齢は' + age +'歳です。');
+console.log('私のニックネームは' + nickname + 'です。年齢は' + age + '歳です。');
 
 // Q2 配列
 let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go',];
 let templateText = `私の好きな言語は${languages[0]}です。次は${languages[3]}を勉強してみたいです。`;
 console.log(templateText);
+
+// Q3 オブジェクト
+let user = {
+  name: 'John',
+  age: 26,
+  bloodType: 'A',
+  favorite: 'card',
+};
+console.log(user.age);
