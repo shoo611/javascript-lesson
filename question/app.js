61681738bf026e18b71f9af8f127440024f18799
shoo611 <itonagasho@itonagashounoMacBook-Air.local>
2025-09-02 14:25:47 +0900

    docs: 応用Q4回答

--- question/app.js
+++ question/app.js
@@ -121,3 +121,10 @@ if (num > 0) {
 } else if (num === 0) {
   console.log('num is 0');
 }
+
+// Q4 for
+let numbers = [];
+for (let i = 0; i <= 99; i++) {
+  numbers[i] = i;
+}
+console.log(numbers);
