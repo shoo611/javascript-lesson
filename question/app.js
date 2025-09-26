2741ba90caf3cdae94d634b62f3bcbd7828405dd
shoo611 <itonagasho@itonagashounoMacBook-Air.local>
2025-09-26 17:56:16 +0900

    docs: 応用Q5回答

--- question/app.js
+++ question/app.js
@@ -128,3 +128,15 @@ for (let i = 0; i <= 99; i++) {
   numbers[i] = i;
 }
 console.log(numbers);
+
+// Q5 for × if
+let mixed = [4, '2', 5, '8', '9', 0, 1];
+for (let i = 0; i < mixed.length; i++) {
+  if (typeof mixed[i] !== 'number') {
+    console.log('not number');
+  } else if(mixed[i] % 2 === 0) {
+    console.log('even');
+  } else {
+    console.log('odd');
+  }
+}
